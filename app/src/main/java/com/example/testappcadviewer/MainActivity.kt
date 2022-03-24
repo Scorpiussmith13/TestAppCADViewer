bfeb9478d1728a3f792c0376b2c38e6fa8c79875
Sanmoy1 <emailofsanmoyroy@gmail.com>
2022-03-24 23:23:18 +0530

    Updates on recyclerView

--- app/src/main/java/com/example/testappcadviewer/MainActivity.kt
+++ app/src/main/java/com/example/testappcadviewer/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.testappcadviewer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var recyclerView: RecyclerView
+    lateinit var photoAdapter: PhotoAdapter
+     private var dataList=mutableListOf<dataModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView=findViewById(R.id.recyclerView)
+        recyclerView.layoutManager=GridLayoutManager(applicationContext,3)
+        photoAdapter= PhotoAdapter(applicationContext)
+        recyclerView.adapter=photoAdapter
+
+        dataList.add(dataModel(R.drawable.profilepicture))
+        dataList.add(dataModel(R.drawable.profilepicture))
+        dataList.add(dataModel(R.drawable.profilepicture))
+        dataList.add(dataModel(R.drawable.profilepicture))
+        dataList.add(dataModel(R.drawable.profilepicture))
+        dataList.add(dataModel(R.drawable.profilepicture))
+        dataList.add(dataModel(R.drawable.profilepicture))
+        dataList.add(dataModel(R.drawable.profilepicture))
+        dataList.add(dataModel(R.drawable.profilepicture))
+        dataList.add(dataModel(R.drawable.profilepicture))
+
+        photoAdapter.setDataList(dataList)
     }
 }
