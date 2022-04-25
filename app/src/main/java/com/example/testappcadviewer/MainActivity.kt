19ca1ded9807cd48d74306178993b69551a4e8c9
Scorpiussmith13 <sourasishdas18@yahoo.com>
2022-04-25 22:14:17 +0530

    Added few changes to UI. Added post button.

--- app/src/main/java/com/example/testappcadviewer/MainActivity.kt
+++ app/src/main/java/com/example/testappcadviewer/MainActivity.kt
@@ -1,23 +1,33 @@
 package com.example.testappcadviewer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var postButton:Button
     lateinit var recyclerView: RecyclerView
     lateinit var photoAdapter: PhotoAdapter
      private var dataList=mutableListOf<dataModel>()
+    companion object{
+        val IMAGE_REQUEST_CODE= 100
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        postButton=findViewById(R.id.PostButton)
         recyclerView=findViewById(R.id.recyclerView)
         recyclerView.layoutManager=GridLayoutManager(applicationContext,3)
         photoAdapter= PhotoAdapter(applicationContext)
         recyclerView.adapter=photoAdapter
-
+        postButton.setOnClickListener { pickImage() }
         dataList.add(dataModel(R.drawable.profilepicture))
         dataList.add(dataModel(R.drawable.profilepicture))
         dataList.add(dataModel(R.drawable.profilepicture))
@@ -28,7 +38,27 @@ class MainActivity : AppCompatActivity() {
         dataList.add(dataModel(R.drawable.profilepicture))
         dataList.add(dataModel(R.drawable.profilepicture))
         dataList.add(dataModel(R.drawable.profilepicture))
-
         photoAdapter.setDataList(dataList)
+
+
     }
+    private fun pickImage()
+    {
+
+        val intent =Intent(Intent.ACTION_PICK)
+        intent.type= "image/*"
+        startActivityForResult(intent, IMAGE_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == IMAGE_REQUEST_CODE && resultCode == RESULT_OK){
+            //imageView.setImage(data?.data)
+
+            Toast.makeText(this,"Opened gallery",Toast.LENGTH_SHORT).show()
+        }
+
+
+    }
+
 }
